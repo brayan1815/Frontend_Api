877e94e5afb340887a93240b5a01f6edc85a4d63
Brayan <brayanfernandezjimenez18@gmail.com>
2025-05-11 15:04:24 -0500

    validacion del formulario de usuarios

---
+++ modules/modulos.js
@@ -0,0 +1,132 @@
+export const validarLetras=(event)=>{
+    let tecla=event.key;
+    const letras=/[a-zñáéíóú\s]/i;
+    if(!letras.test(tecla)&& tecla!="Backspace"){
+        event.preventDefault();
+    }
+}
+
+export const validarNumeros=(event)=>{
+    let tecla=event.key;
+    const numeros=/[0-9]/;
+    if(!numeros.test(tecla) && tecla!="Backspace"){
+    event.preventDefault();
+  }
+}
+
+export const validar=(event)=>{
+    event.preventDefault();
+
+    const campos=[...event.target].filter((campo)=>{
+        if(campo.hasAttribute("required")) return campo;
+    })
+    console.log(campos);
+    campos.forEach(campo=>{
+        let tipo=campo.tagName;
+        console.log(tipo);
+        
+        switch(tipo){
+            case "INPUT":
+                if(campo.value==""){
+                    campo.classList.add("border-red");
+                    campo.focus();
+                    if(campo.nextElementSibling) campo.nextElementSibling.remove();
+                    let afterend=document.createElement('span');
+                    afterend.textContent=`El campo ${campo.getAttribute("name")} no puede estar vacio`;
+                    campo.insertAdjacentElement('afterend',afterend);
+                }
+                break;
+            case "SELECT":
+                if (campo.selectedIndex == 0) {
+                    campo.classList.add("border-red");
+                    if (campo.nextElementSibling) campo.nextElementSibling.remove();
+                    let afterend = document.createElement('span');
+                    afterend.textContent = "Debe seleccionar un elemento"
+                    campo.insertAdjacentElement('afterend', afterend);
+                }
+                break;
+        }
+    })
+
+    const radios=campos.filter((campo)=>{
+        if(campo.type=="radio")return campo;
+    })
+
+    const validarChequeoRdios=(radios)=>{
+        for(let n=0;n<radios.length;n++){
+            if(radios[n].checked)return true;
+        }
+        return false;
+    }
+
+    console.log(validarChequeoRdios(radios))
+
+    if(validarChequeoRdios(radios)==false){
+        let par=radios[0].parentElement;
+        let contenRadios=par.parentElement;
+        contenRadios.classList.add("border-red");
+        if (contenRadios.nextElementSibling) contenRadios.nextElementSibling.remove();
+        let afterend = document.createElement('span');
+        afterend.textContent = "Debe seleccionar un genero"
+        contenRadios.insertAdjacentElement('afterend', afterend);
+    }
+
+    const cheboxs=campos.filter((campo)=>{
+        if(campo.type=="checkbox")return campo;
+    })
+
+    const cheboxs_seleccionados=cheboxs.filter((checbox) => checbox.checked)
+    console.log(cheboxs_seleccionados)
+    if(cheboxs_seleccionados.length<3){
+        let pad=cheboxs[0].parentElement;
+        let padre_checboxs=pad.parentElement;
+        padre_checboxs.classList.add("border-red")
+        if (padre_checboxs.nextElementSibling) padre_checboxs.nextElementSibling.remove();
+        let afterend = document.createElement('span');
+        afterend.textContent = "Debe seleccionar minimo 3 lenguajes"
+        padre_checboxs.insertAdjacentElement('afterend', afterend);
+    }
+}
+
+export const limpiar = (event) => {
+    if (event.target.value != "" && event.target.selectedIndex!=0) {
+      event.target.classList.remove("border-red");
+      if (event.target.nextElementSibling) {
+        event.target.nextElementSibling.remove();
+      }
+    }
+}
+
+export const limpiarRadios=(event)=>{
+    let radio=event.target;
+    let pad=radio.parentElement;
+    let padreRadios=pad.parentElement;
+    padreRadios.classList.remove("border-red");
+    if (padreRadios.nextElementSibling) {
+        padreRadios.nextElementSibling.remove();
+      }
+}
+
+export const limpiarChecboxs=(event)=>{
+    let chec=event.target.parentElement;
+    let checboxsContenedor=chec.parentElement;
+    let che=checboxsContenedor.childNodes;
+
+    let cheboxs=[];
+
+    che.forEach(element => {
+        if(element.tagName=="DIV"){
+            element.childNodes.forEach(element => {
+                if(element.tagName=="INPUT")cheboxs.push(element);
+            });
+        }
+    });
+    
+    const cheboxs_seleccionados=cheboxs.filter((checbox) => checbox.checked)
+    if(cheboxs_seleccionados.length>=3){
+        checboxsContenedor.classList.remove("border-red");
+        if (checboxsContenedor.nextElementSibling) {
+            checboxsContenedor.nextElementSibling.remove();
+        }
+    }
+}
--- validation/usuarios.js
+++ validation/usuarios.js
@@ -1,11 +1,32 @@
-import { validarLetras, validarNumeros } from "../modules/usuarios_modules.js";
+import { limpiar, limpiarChecboxs, limpiarRadios, validar, validarLetras, validarNumeros } from "../modules/modulos.js";
 
 const formulario=document.querySelector("form");
-const documento_usuario=document.querySelector('[name="nombre"]')
-const nombre_usuario=document.querySelector('[name="nombre"]')
-const apellido_usuario=document.querySelector('[name="apellido"]')
+const documento_usuario=document.querySelector('[name="documento"]');
+const nombre_usuario=document.querySelector('[name="nombre"]');
+const apellido_usuario=document.querySelector('[name="apellido"]');
+const telefono_usuario=document.querySelector('#telefono');
+const constrasenia_usuario=document.querySelector('[name="contrasenia"]')
+const ciudad_usuario=document.querySelector('[name="ciudad"]')
+const radios_genero=document.querySelectorAll('[name="genero"]')
+const cheboxs_lenguajes=document.querySelectorAll('[name="lenguaje"]')
 
 
-documento_usuario.addEventListener('keydown',validarNumeros)
+
+
+formulario.addEventListener('submit',validar)
+documento_usuario.addEventListener('keydown',validarNumeros);
 nombre_usuario.addEventListener('keydown',validarLetras);
-apellido_usuario.addEventListener('keydown',validarLetras)
+apellido_usuario.addEventListener('keydown',validarLetras);
+telefono_usuario.addEventListener('keydown',validarNumeros);
+documento_usuario.addEventListener('blur',limpiar)
+nombre_usuario.addEventListener('blur',limpiar)
+apellido_usuario.addEventListener('blur',limpiar)
+telefono_usuario.addEventListener('blur',limpiar)
+constrasenia_usuario.addEventListener('blur',limpiar)
+ciudad_usuario.addEventListener('blur',limpiar)
+radios_genero.forEach(radio => {
+    radio.addEventListener('change',limpiarRadios)
+});
+cheboxs_lenguajes.forEach(chec=>{
+    chec.addEventListener('change',limpiarChecboxs)
+})
